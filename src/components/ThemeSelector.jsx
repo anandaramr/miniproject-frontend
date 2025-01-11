7bbf3150c85ad335d0b492919d396ce63c096e59
Anandaram R <anandaramr1915@gmail.com>
2025-01-11 15:45:42 +0530

    Minor style changes for theme selector

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -1,20 +1,25 @@
 import { getTheme, handleChangeTheme } from "../utils/theme";
 
-export default function ThemeSelector() {
+export default function ThemeSelector({ setTheme }) {
 
     const theme = getTheme()
+
+    function changeTheme(evt) {
+        handleChangeTheme(evt)
+        setTheme(document.documentElement.classList.contains('dark') ? 'dark' : 'light')
+    }
     
     return (
-        <div className="dark:border-zinc-700 border-zinc-300 shadow-md border-2 rounded-lg m-3 p-5 w-fit">
-				<form onChange={handleChangeTheme} className="">
+        <div className="dark:border-zinc-700 border-zinc-300 shadow-md border-[1px] rounded-lg my-2 pr-3 pl-5 py-4 w-fit">
+				<form onChange={changeTheme} className="text-base">
 					<input className="border-2 border-cyan-400 w-3 h-3 rounded-full checked:bg-cyan-400 duration-200 appearance-none" defaultChecked={theme=='dark'} name="theme" type="radio" id="dark" />
-					<label className="px-3 text-lg" htmlFor="dark">Dark</label><br></br>
+					<label className="px-3" htmlFor="dark">Dark</label><br></br>
 
 					<input className="border-2 border-cyan-400 w-3 h-3 rounded-full checked:bg-cyan-400 duration-200 appearance-none" defaultChecked={theme=='light'} name="theme" type="radio" id="light" />
-					<label className="px-3 text-lg" htmlFor="light">Light</label><br></br>
+					<label className="px-3" htmlFor="light">Light</label><br></br>
 
 					<input className="border-2 border-cyan-400 w-3 h-3 rounded-full checked:bg-cyan-400 duration-200 appearance-none" defaultChecked={theme=='system'} name="theme" type="radio" id="system" />
-					<label className="px-3 text-lg" htmlFor="system">System</label><br></br>
+					<label className="px-3" htmlFor="system">System</label><br></br>
 				</form>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import { cls } from "../utils/cls"
 import ThemeSelector from "../components/ThemeSelector"
 import Popup from "../components/Popup"
+import { useEffect, useState } from "react"
 
 function Home() {
 
+	const [ theme, setTheme ] = useState(document.documentElement.classList.contains('dark') ? 'dark' : 'light')
+
+	useEffect(() => {
+		const theme = document.querySelector('#theme')
+		theme.animate([ { transform: 'rotate(60deg)' }, { transform: 'rotate(0deg)' } ], { duration: 200, iterations: 1 })
+	}, [theme])
+
 	return (
 		<div className={cls('p-5', 'text-xl')}>
-			<Popup title="Theme">
-				<ThemeSelector />
+			<Popup title={<span id="theme" className="material-symbols-outlined dark:text-zinc-300 text-zinc-700 text-2xl">{ theme=='dark' ? 'dark_mode' : 'light_mode'}</span>}>
+				<ThemeSelector setTheme={setTheme} />
 			</Popup>
 		</div>
 	)
