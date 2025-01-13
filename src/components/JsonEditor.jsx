3befded119d8958d4466dae6d0b41055b115ee1c
Devika Krishnan M <dkrishnanm@gmail.com>
2025-01-13 20:20:43 +0530

    Carried out minor style changes

--- src/components/JsonEditor.jsx
+++ src/components/JsonEditor.jsx
@@ -1,28 +1,41 @@
-import CodeMirror from "@uiw/react-codemirror"
+import CodeMirror, { EditorView } from "@uiw/react-codemirror"
 import { json, jsonParseLinter } from "@codemirror/lang-json"
 import { editorDarkTheme, editorLightTheme } from "../utils/codemirror-theme"
 import { linter } from "@codemirror/lint"
-import { useCallback, useContext } from "react";
+import { useCallback, useContext} from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 export default function JsonEditor({ value, setValue, readOnly, height, width }) {
 
+    
     const onChange = useCallback((val) => {
-        setValue(val);
+        if(setValue) setValue(val);
     }, []);
-
+    
+    const editorTheme = EditorView.theme({
+        "&.cm-focused" : {
+            "outline" : "none"
+        }
+    })
     const { theme } = useContext(ThemeContext)
+    
+    const lint = linter(jsonParseLinter());
+    const extensions =[json(), editorTheme]
+    if(!readOnly) extensions.push(lint)
+
 
     return (
-        <CodeMirror
-            className="mx-5 border-[0.5px] border-zinc-800 text-sm w-fit"
-            value={value}
-            theme={theme == "dark" ? editorDarkTheme : editorLightTheme}
-            height={height || "500px"}
-            width={width || "500px"}
-            extensions={[json(), linter(jsonParseLinter())]}
-            onChange={onChange}
-            editable={!readOnly}
-        />
+        <div className="text-sm w-fit border-[1px] border-zinc-800 h-fit my-10 rounded-lg p-1">
+            <CodeMirror
+                className="border-zinc-900 outline-none"
+                value={value}
+                theme={theme == "dark" ? editorDarkTheme : editorLightTheme}
+                height={height || "500px"}
+                width={width || "500px"}
+                extensions={extensions}
+                onChange={onChange}
+                editable={!readOnly}
+            />
+        </div>
     );
 }
