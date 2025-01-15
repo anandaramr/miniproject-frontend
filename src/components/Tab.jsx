22afd7a8e06c69f52fd37acf97ad03a04e2b5dae
Anandaram R <anandaramr1915@gmail.com>
2025-01-16 01:43:04 +0530

    Add tab rename functionality and minor refactoring

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,17 +1,44 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { cls } from "../utils/cls";
+import { updateState } from "../utils/utils";
 
-export default function Tab({ title, active, onClick, close }) {
+export default function Tab({ id, title, active, onClick, close }) {
 
     function closeSelf(evt) {
         evt.stopPropagation()
         close()
     }
 
+    const [ tabName, setTabName ] = useState(title)
+    const [ renameMode, setRenameMode ] = useState(false)
+
+    function rename(newName) {
+        updateState(({ tabs }) => {
+            tabs = tabs.map(item => {
+                if (item.tabId==id) {
+                    item.title = newName
+                }
+                return item
+            })
+            return { tabs }
+        })
+        setRenameMode(false)
+        setTabName(newName.trim())
+    }
+
+    function handleKeyDown(evt) {
+        if (evt.code=='Escape') {
+            setRenameMode(false)
+        } else if (evt.code=='Enter') {
+            rename(evt.target.value)
+        }
+    }
+
     return (
-        <div draggable onClick={onClick} className={cls("gap-5 flex items-center border-[1px] dark:border-zinc-800 rounded-sm px-3 py-1 cursor-pointer select-none", active && "bg-zinc-300 dark:bg-zinc-800")}>
-            <span className={cls(active ? "dark:text-zinc-400 text-zinc-800" : "text-zinc-500")}>{title || "Untitled"}</span>
-            {close && <span onClick={closeSelf} className="text-base hover:text-rose-400 duration-200 text-zinc-500 material-symbols-outlined">close</span>}
+        <div draggable={!renameMode} onClick={onClick} className={cls("gap-5 flex items-center border-[1px] dark:border-zinc-800 rounded-sm px-3 py-1 cursor-pointer select-none", active && "bg-zinc-300 dark:bg-zinc-800")}>
+            {!renameMode && <span onDoubleClick={() => setRenameMode(true)} className={cls("w-[60px]", active ? "dark:text-zinc-400 text-zinc-800" : "text-zinc-500")}>{tabName || "Untitled"}</span>}
+            {renameMode && <input className="w-[60px] bg-transparent text-zinc-800 dark:text-zinc-400 caret-zinc-800 dark:caret-white outline-none" onClick={rename} onBlur={() => setRenameMode(false)} onKeyDown={handleKeyDown} defaultValue={tabName || "Untitled"} spellCheck={false} autoFocus  />}
+            <span onClick={closeSelf} className="text-base hover:text-rose-400 duration-200 text-zinc-500 material-symbols-outlined">close</span>
         </div>
     )
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,14 @@
 import { v4 as uuid } from 'uuid'
 
+export function updateState(func) {
+    const { data } = parseJson(localStorage.getItem("state"))
+    let { tabs, lastActiveTab } = func(data || {})
+    
+    tabs = tabs || data?.tabs
+    lastActiveTab = lastActiveTab || data?.lastActiveTab
+    localStorage.setItem("state", JSON.stringify({ lastActiveTab, tabs }))
+}
+
 export function parseJson(body) {
     try {
         const data = JSON.parse(body)
