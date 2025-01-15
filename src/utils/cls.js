ea36565822ba273178e4aaf5327a1f2139b54f07
Anandaram R <anandaramr1915@gmail.com>
2025-01-16 02:17:41 +0530

    Write documentation for utils functions

--- src/utils/cls.js
+++ src/utils/cls.js
@@ -20,6 +20,16 @@ class ClassNames {
 
 }
 
+/**
+ * A utility function to create a class name string
+ *
+ * @param {...string} classNames - Class names to add
+ * @returns {string} A space-separated string of unique class names
+ *
+ * @example
+ * const active = false
+ * const classes = cls('text-lg bg-white', 'select-none', active ? 'text-emerald-300' : 'text-red-500'); // "text-lg bg-white select-none text-red-500"
+ */
 export function cls(...classNames) {
     return new ClassNames().add(...classNames)
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,27 @@
 import { v4 as uuid } from 'uuid'
 
+/**
+ * Updates state information stored in `localStorage`
+ *
+ * @param {function} func - Function which take as input the current state and outputs updates if any
+ * 
+ * - The argument and return value of `func` is an object where:
+ *    - `tabs` (optional): The updated list of tabs.
+ *    - `lastActiveTab` (optional): The updated value for the last active tab.
+ * 
+ * If `func` does not provide these values, the existing values from the current state will be retained.
+ *
+ * The updated state is saved back to `localStorage`.
+ * 
+ * @example
+ * // Add new tab
+ * updateState(({ tabs }) => {
+ *     return {
+ *         tabs: [ ...tabs, newTab ],
+ *         lastActiveTab: newTab.tabId
+ *     }
+ * })
+ */
 export function updateState(func) {
     const { data } = parseJson(localStorage.getItem("state"))
     let { tabs, lastActiveTab } = func(data || {})
@@ -9,6 +31,22 @@ export function updateState(func) {
     localStorage.setItem("state", JSON.stringify({ lastActiveTab, tabs }))
 }
 
+/**
+ * Safely parse a JSON string and returns the parsed data if succesfully parsed; else it return an error.
+ *
+ * @param {string} body - The JSON string to parse.
+ * @returns {{ data?: any, error?: Error }} An object containing either the parsed `data` or an `error`.
+ *
+ * @example
+ * // Valid JSON string
+ * const result = parseJson('{"key": "value"}');
+ * console.log(result.data); // { key: "value" }
+ *
+ * @example
+ * // Invalid JSON string
+ * const result = parseJson('invalid json');
+ * console.log(result.error);
+ */
 export function parseJson(body) {
     try {
         const data = JSON.parse(body)
